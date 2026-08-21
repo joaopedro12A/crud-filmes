dec244f1cefabddf02a63438be7708eac24bc1cb
joaopedro12A <161172343+joaopedro12A@users.noreply.github.com>
2026-08-21 12:41:45 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,45 @@
+async function buscarFilmes() {
+    try {
+        const resposta = await fetch("/filmes");
+
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar filmes");
+        }
+
+        const filmes = await resposta.json();
+
+        const sectionFilmes = document.querySelector(".filmes");
+
+        sectionFilmes.innerHTML = "";
+
+        filmes.forEach((filme) => {
+            sectionFilmes.innerHTML += `
+                <div class="filme">
+
+                    <h2>${filme.titulo}</h2>
+
+                    <p>
+                        <strong>Gênero:</strong>
+                        ${filme.genero}
+                    </p>
+
+                    <p>
+                        <strong>Duração:</strong>
+                        ${filme.duracao} minutos
+                    </p>
+
+                    <p>
+                        <strong>Classificação indicativa:</strong>
+                        ${filme.classificacao}
+                    </p>
+
+                </div>
+            `;
+        });
+
+    } catch (erro) {
+        console.error("Erro:", erro);
+    }
+}
+
+buscarFilmes();
